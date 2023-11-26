10799a0a4e2980dd0eaf0f087d3a6e9c990425a0
Khaledtf Labeb <khaledtf19@gmail.com>
2023-11-26 20:35:54 +0200

    adding search by id / name

--- src/crud_todo.rs
+++ src/crud_todo.rs
@@ -3,7 +3,7 @@ use anyhow::Result;
 use sqlx::PgPool;
 use sqlx::Row;
 
-use crate::args::{DoneType,DoneCommand, DoneNameCommand, DoneIdCommand};
+use crate::args::{DoneCommand, DoneIdCommand, DoneNameCommand, DoneType};
 use crate::structs::Todo;
 
 pub async fn create_todo(name: String, discription: String, pool: &PgPool) -> Result<()> {
@@ -15,20 +15,35 @@ pub async fn create_todo(name: String, discription: String, pool: &PgPool) -> Re
     Ok(())
 }
 
-pub async fn done_todo(done: DoneCommand, pool: &PgPool) -> Result<()>{
+pub async fn done_todo(done: DoneCommand, pool: &PgPool) -> Result<()> {
     match done.done {
         DoneType::Id(DoneIdCommand { id }) => {
             let q = "SELECT * FROM todos WHERE id = $1;";
-            let query = sqlx::query_as::<_,Todo>(q).bind(id);
+            let query = sqlx::query_as::<_, Todo>(q).bind(id);
             let todo = query.fetch_optional(pool).await?;
             println!("{:?}", todo);
         }
         DoneType::Name(DoneNameCommand { name }) => {
             let q = "SELECT * FROM todos WHERE name LIKE $1";
-            let query = sqlx::query_as::<_, Todo>(q).bind(name+"%");
+            let query = sqlx::query_as::<_, Todo>(q).bind(name + "%");
             let todos = query.fetch_all(pool).await?;
             println!("{:?}", todos);
         }
     }
+    Ok(())
+}
+
+pub async fn search_todo_by_id(id: i32, pool: &PgPool) -> Result<()> {
+    let q = "SELECT * FROM todos WHERE id = $1;";
+    let query = sqlx::query_as::<_, Todo>(q).bind(id);
+    let todo = query.fetch_optional(pool).await?;
+    println!("{:?}", todo);
+    Ok(())
+}
+pub async fn search_todo_by_name(name: String, pool: &PgPool) -> Result<()> {
+    let q = "SELECT * FROM todos WHERE name LIKE $1";
+    let query = sqlx::query_as::<_, Todo>(q).bind(name + "%");
+    let todos = query.fetch_all(pool).await?;
+    println!("{:?}", todos);
     Ok(())
 }
